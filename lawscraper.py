cb18c6766e8dcf1fe955f101c81e2cc2ab19fa17
BryanTZY <bryantan.2019@law.smu.edu.sg>
2020-12-10 18:38:42 +0800

    first edits

--- lawscraper.py
+++ lawscraper.py
@@ -1,7 +1,26 @@
 from bs4 import BeautifulSoup
 import re
-import spacy
 import json
 import requests 
+import urllib.request
+
+url = "https://lawgazette.com.sg/archives"
+
+try:
+    archive = requests.get(url, headers= {'Accept': 'application/vnd.github.v3.text-match+json'})
+    
+except:
+    print("An error occurred.")
+
+# print(page.headers)
+
+soup = BeautifulSoup(archive.text)
+print(soup)
+
+regex = re.compile('^issue-block-')
+content_lis = soup.find_all('li', attrs={'class': regex})
+print(content_lis)
+
+
 
 
