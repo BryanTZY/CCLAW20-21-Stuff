e592da4a7779524b713599c7d86ccba451b191ac
BryanTZY <bryantan.2019@law.smu.edu.sg>
2020-12-13 00:57:49 +0800

    added case citation number scraping, which is appended to case name in the case_dict

--- sgsc.py
+++ sgsc.py
@@ -12,18 +12,21 @@ headers= {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Geck
 def sup_court_scraper():
 
     root_url = "https://www.supremecourt.gov.sg/news/supreme-court-judgments/page/"
+    case_dict = dict()
+
     url = root_url + '1'
     results = requests.get(url, headers=headers)
     soup = BeautifulSoup(results.text, "html5lib")
     
-    case_dict = dict()
     boxes = soup.find_all('div', class_="judgmentpage")
+    total_cases = int(soup.find('div', class_="amount").get_text().strip(' \t\n')[:4])
+    print(total_cases)
     
     for i in boxes:
         text = i.find('div', class_="text").find_all(text=True, recursive=False)
-        casename = re.sub('[\t\n]', '', text[1]).strip(' ')
+        caseref = i.find('ul', class_="decision").find('li').get_text() #neutral citation
+        casename = re.sub('[\t\n]', '', text[1]).strip(' ') + ' ' + caseref
         pdf_link = i.find('div', class_="download")
-        # print(pdf_link.a['href'])
         case_dict[casename] = pdf_link.a['href']
 
     for k, v in case_dict.items():
