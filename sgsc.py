d264a53a3c2a7795bd4712ded0fed128a722d583
BryanTZY <bryantan.2019@law.smu.edu.sg>
2020-12-13 03:12:00 +0800

    partial commit. Regex to cleanly remove paragraph numbers needs touching up

--- sgsc.py
+++ sgsc.py
@@ -5,6 +5,8 @@ import requests
 import spacy
 import math
 import io
+import random
+from PyPDF2 import PdfFileReader
 
 headers= {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
@@ -12,10 +14,9 @@ headers= {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Geck
 def sup_court_scraper():
 
     root_url = "https://www.supremecourt.gov.sg/news/supreme-court-judgments/page/"
-    root_pdf_url = "https://www.supremecourt.gov.sg" #use later to construct the judgment pdf link
     case_dict = dict()
 
-    url = root_url + '23'
+    url = root_url + str(random.randint(1, 200)) #random page for testing purposes
     results = requests.get(url, headers=headers)
     soup = BeautifulSoup(results.text, "html5lib")
     
@@ -34,15 +35,57 @@ def sup_court_scraper():
     # page_count = math.ceil(total_cases/case_count)
     page_count = 20 #for testing purposes
 
-    # for i in range(1, page_count):
+    # for i in range(1, page_count + 1):
     #     scrape_numbered_page(i, case_dict)
     
-    for k, v in case_dict.items():
-        print(k, v, '\n')
+    # print("Now printing case dictionary...")
+    # for k, v in case_dict.items():
+    #     print(k + ",", v,)
+    # print()
+
+    casenames = [k for k, v in case_dict.items()]
+    casetext_dict = dict() #store lists of sentences of every judgment
+    random_casename = random.choice(casenames)
+    print(random_casename)
+    pdf_scraper(case_dict[random_casename], casetext_dict)
+
 
     return
 
-def scrape_numbered_page(pageno, case_dict): #pageno should be int
+def pdf_scraper(pdf_url_fragment, casetext_dict):
+    root_pdf_url = "https://www.supremecourt.gov.sg" 
+
+    r = requests.get(root_pdf_url+pdf_url_fragment)
+    f = io.BytesIO(r.content)
+    reader = PdfFileReader(f)
+
+    total_pages = reader.getNumPages()
+    print("Total pages:", total_pages)
+
+    contents = reader.getPage(6).extractText()
+
+    substitutions = [[r'(?<=\n\s)[0-9]*', ' '],['\n', ' '], [r"[ﬂﬁ]", '"'], ['™', "'"], ['\t', 'magic'] ] #hardcoded substitutions where pdfreader failed to susbtitute correctly
+    #Unresolved sentence issues
+    #(1) Judgment numbers (2) ". [emphasis in original]" (3) O. 18 r. 7 (wrong??)
+   
+    for x in substitutions:
+        contents = re.sub(x[0], x[1], contents)
+    print(contents, '\n')
+    # contents1 = contents.split('\n')
+    # for i in contents1:
+    #     print("**"+ i + "**")
+    #     print()
+
+    contents2 = re.split(r'(?<=\w\.)\s', contents)
+    for i in contents2: 
+        print(i, '\n')
+    print("Total 'sentences':", len(contents2))
+
+
+    return
+
+
+def scrape_numbered_page(pageno, case_dict): #sub-function to scrape a page of judgments, given page number
 
     root_url = "https://www.supremecourt.gov.sg/news/supreme-court-judgments/page/"
     root_pdf_url = "https://www.supremecourt.gov.sg" #use later to construct the judgment pdf link
@@ -61,9 +104,9 @@ def scrape_numbered_page(pageno, case_dict): #pageno should be int
         pdf_link = i.find('a', class_ = "pdf-download")
         case_dict[casename] = pdf_link['href']
 
-    for k, v in case_dict.items():
-        print(k, v, '\n')
-
+    # for k, v in case_dict.items():
+    #     print(k, v, '\n')
+    print("Page", pageno, "done...")
     return
 
 def sup_court_pinpoint_scraper():
