e907d4a6b22b39c2f619e3caadb919cd763da503
BryanTZY <bryantan.2019@law.smu.edu.sg>
2020-12-13 01:19:57 +0800

    fixed bug where link to media summary was scraped instead. Also created sub-function which enables pages of judgments to be looped iteratively

--- sgsc.py
+++ sgsc.py
@@ -4,7 +4,7 @@ import json
 import requests 
 import spacy
 import math
-import random #for testing purposes
+import io
 
 headers= {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
@@ -12,15 +12,74 @@ headers= {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Geck
 def sup_court_scraper():
 
     root_url = "https://www.supremecourt.gov.sg/news/supreme-court-judgments/page/"
+    root_pdf_url = "https://www.supremecourt.gov.sg" #use later to construct the judgment pdf link
     case_dict = dict()
 
-    url = root_url + '1'
+    url = root_url + '23'
+    results = requests.get(url, headers=headers)
+    soup = BeautifulSoup(results.text, "html5lib")
+    
+    boxes = soup.find_all('div', class_="judgmentpage")
+    total_cases = int(soup.find('div', class_="amount").get_text().strip(' \t\n')[:4])
+    case_count = 0
+    
+    for i in boxes:
+        text = i.find('div', class_="text").find_all(text=True, recursive=False)
+        caseref = i.find('ul', class_="decision").find('li').get_text() #neutral citation
+        casename = re.sub('[\t\n]', '', text[1]).strip(' ') + ' ' + caseref
+        pdf_link = i.find('a', class_ = "pdf-download")
+        case_dict[casename] = pdf_link['href']
+        case_count += 1
+
+    # page_count = math.ceil(total_cases/case_count)
+    page_count = 20 #for testing purposes
+
+    # for i in range(1, page_count):
+    #     scrape_numbered_page(i, case_dict)
+    
+    for k, v in case_dict.items():
+        print(k, v, '\n')
+
+    return
+
+def scrape_numbered_page(pageno, case_dict): #pageno should be int
+
+    root_url = "https://www.supremecourt.gov.sg/news/supreme-court-judgments/page/"
+    root_pdf_url = "https://www.supremecourt.gov.sg" #use later to construct the judgment pdf link
+
+    url = root_url + str(pageno)
+    results = requests.get(url, headers=headers)
+    soup = BeautifulSoup(results.text, "html5lib")
+    
+    boxes = soup.find_all('div', class_="judgmentpage")
+    total_cases = int(soup.find('div', class_="amount").get_text().strip(' \t\n')[:4])
+    
+    for i in boxes:
+        text = i.find('div', class_="text").find_all(text=True, recursive=False)
+        caseref = i.find('ul', class_="decision").find('li').get_text() #neutral citation
+        casename = re.sub('[\t\n]', '', text[1]).strip(' ') + ' ' + caseref
+        pdf_link = i.find('a', class_ = "pdf-download")
+        case_dict[casename] = pdf_link['href']
+
+    for k, v in case_dict.items():
+        print(k, v, '\n')
+
+    return
+
+def sup_court_pinpoint_scraper():
+
+    root_url = "https://www.supremecourt.gov.sg/news/supreme-court-judgments/page/"
+    root_pdf_url = "https://www.supremecourt.gov.sg" #use later to construct the judgment pdf link
+    case_dict = dict()
+
+    url = root_url + '23'
     results = requests.get(url, headers=headers)
     soup = BeautifulSoup(results.text, "html5lib")
     
     boxes = soup.find_all('div', class_="judgmentpage")
     total_cases = int(soup.find('div', class_="amount").get_text().strip(' \t\n')[:4])
     print(total_cases)
+    case_count = 0
     
     for i in boxes:
         text = i.find('div', class_="text").find_all(text=True, recursive=False)
@@ -28,10 +87,11 @@ def sup_court_scraper():
         casename = re.sub('[\t\n]', '', text[1]).strip(' ') + ' ' + caseref
         pdf_link = i.find('div', class_="download")
         case_dict[casename] = pdf_link.a['href']
+        case_count += 1
 
     for k, v in case_dict.items():
         print(k, v, '\n')
-
+    page_count = math.ceil(total_cases/case_count) #FYI
 
     return
 
