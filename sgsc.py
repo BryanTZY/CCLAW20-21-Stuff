5be048f455589d02b5c736cfe77b5078341c0873
BryanTZY <bryantan.2019@law.smu.edu.sg>
2020-12-15 10:38:34 +0800

    temp commit

--- sgsc.py
+++ sgsc.py
@@ -8,6 +8,11 @@ import io
 import random
 import pdfplumber
 
+#headers - pick them out
+#quotes - 
+#Scraping 
+#lists - recognising judge names
+
 headers= {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
 
