71dcb9a29f03aaecfd166ee61475c050446e3434
BryanTZY <bryantan.2019@law.smu.edu.sg>
2020-12-17 14:42:43 +0800

    temp commit

--- sgsc.py
+++ sgsc.py
@@ -1,17 +1,14 @@
 from bs4 import BeautifulSoup
 import re
-import json
+# import json
 import requests 
-import spacy
+
 import math
 import io
 import random
 import pdfplumber
+import spacy
 
-#headers - pick them out
-#quotes - 
-#Scraping 
-#lists - recognising judge names
 
 headers= {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
