4ba3eeabf173d6e396c0a3fdfd5185e2cee9c903
BryanTZY <bryantan.2019@law.smu.edu.sg>
2020-12-29 22:26:33 +0800

    minor change

--- sgsc3.py
+++ sgsc3.py
@@ -7,7 +7,7 @@ import dataclasses
 import sys
 import datetime
 import os
-from typing import List, Any 
+from typing import List, Any
 
 headers= {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
